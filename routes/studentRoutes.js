cc485b02cd4aec3783ccd3932dc929ea6109039a
Izuchukwu Alaneme <izuchi.alaneme@gmail.com>
2024-11-21 14:24:26 +0100

    Added admin and auth to endpoints

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const router = express.Router()
 
-const { createStudent, loginStudent, assignClassToStudent, getAllStudent } = require("../controllers/studentController");
-const { createClassroom, getAllClassrooms } = require("../controllers/classroomController");
+//import middlewares
+const { adminMiddleware } = require('../middlewares/adminMiddleware');
+const { authMiddleware } = require('../middlewares/authMiddleware');
+
+// Import Controllers
+const { createStudent, loginStudent, assignClassToStudent, getAllStudent, updateStudentProfile, logoutStudent } = require("../controllers/studentController");
+
 
 // Creating Endpoints for students.
-router.post("/student/register", createStudent);
-router.post("/student/login", loginStudent);
-router.patch("/assign-Class", assignClassToStudent)
-router.get("/students", getAllStudent)
+router.post("/student/login", loginStudent);  // Login Student
+router.patch("/update-Student/:studentId", authMiddleware, updateStudentProfile); // Update Student Profile
+router.post("/student/logout", authMiddleware, logoutStudent); // Logout 
 
 
 
-//Creating Endpoints for classrooms
-router.post('/create-classroom', createClassroom);
-router.get('/classrooms', getAllClassrooms);
+
+// Admin routes
+router.post("/student/register", authMiddleware, adminMiddleware,  createStudent);
+router.get("/students", authMiddleware, adminMiddleware, getAllStudent);
+router.patch("/assign-Class", authMiddleware, adminMiddleware, assignClassToStudent);
+
 
 module.exports = router;
