d7798a81f0abaaa81d4e4b00f603d839aca6f593
Izuchukwu Alaneme <izuchi.alaneme@gmail.com>
2025-02-18 13:20:39 +0100

    Worked on merging the news and event controllers and routes. also created the various news controllers

--- controllers/eventAndNewsController.js
+++ controllers/eventAndNewsController.js
@@ -1,7 +1,7 @@
 const Event = require("../models/Event");
 const cloudinary = require("../utilities/cloudinary");
 const fs = require("fs");
-const New = require("../models/New");
+const News = require("../models/New");
 
 // Events and News
 
@@ -174,7 +174,7 @@ exports.createNews = async (req, res) => {
   }
 
   try {
-    const newNews = new New({ title, content });
+    const newNews = new News({ title, content });
 
     await newNews.save();
 
@@ -184,12 +184,98 @@ exports.createNews = async (req, res) => {
   }
 }
 
-// Get all news
+// Get all news ...... Working
 exports.getAllNews = async (req, res) => {
   try {
-    const news = await New.find().sort({ createdAt: -1 });
+    const news = await News.find().sort({ createdAt: -1 });
     res.status(200).json(news);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 }
+
+// Get latest news for Home page 3 news...... Working
+exports.getLatestNews = async (req, res) => {
+  try {
+    const news = await News.find().sort({ createdAt: -1 }).limit(3);
+    res.status(200).json(news);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+// Get latest news for news page..... Working
+exports.getLatestNewsPage = async (req, res) => {
+  try {
+    const news = await News.find().sort({ createdAt: -1 }).limit(15);
+    res.status(200).json(news);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+// Delete a news........ Working
+exports.deleteNews = async (req, res) => {
+  const { newsId } = req.params;
+
+
+  try {
+    const deletedNews = await News.findByIdAndDelete(newsId);
+
+    if (!deletedNews) {
+      return res.status(404).json({ error: "News not found" });
+    }
+
+    res.status(200).json({ message: "News deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Update A News........ Working
+exports.updateNews = async (req, res) => {
+
+  const { newsId } = req.params;
+  const { title, content } = req.body;
+
+  try {
+    
+    const existingNews = await News.findById(newsId);
+    if (!existingNews) {
+      return res.status(404).json({ error: "News not found" });
+    }
+
+   
+    // Update the event
+    const updatedEvent = await News.findByIdAndUpdate(
+      newsId,
+      { title, content },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "News updated successfully",
+      updatedEvent,
+    });
+
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+
+// Get single news...... Working
+exports.getSingleNews = async (req, res) => {
+ const { newsId } = req.params
+
+try {
+  const existingNews = await News.findById(newsId)
+
+  if (!existingNews) {
+    return res.status(404).json({ error: "News with ID " + newsId + " not found"});
+  }
+  
+  res.status(200).json(existingNews);
+} catch (error) {
+  console.error(error, error.message)
+}}
